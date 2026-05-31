e703f610cef20eff37a3aa7906bf6780f31d9526
Abhishek Anand <sauravk8825@gmail.com>
2026-05-31 17:35:23 +0530

    message

---
+++ maze.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void helper(){
+    int moveRight=0;
+    if(moveRight>4){
+        moveRight++;
+
+    }
+}
+int maze(int right,int down){
+    if(right==4 && down==3){
+
+    }
+
+}
+int main(){
+
+}
